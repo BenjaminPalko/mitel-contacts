bf617cc07af3001ea111418d994d990f12f68efa
Benjamin <benjaminpalko@hotmail.com>
2020-02-05 16:11:28 -0500

    Database connection & Post/Get
    - Backend server functionality completed with post/get
    - front-end form submits to backend and retrieves list of contacts

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,60 @@
-let http = require('http');
-let url = require('url');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
 
-http.createServer(function (req, res) {
-    requestListener(req, res)
-}).listen(8080);
+const Pool = require('pg').Pool;
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_SCHEMA,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
+});
 
+app.use(
+    bodyParser.json(),
+    bodyParser.urlencoded({
+        extended: true,
+    }));
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
-function requestListener(request, response) {
-    // This is how you get parameters
-    let params = url.parse(request.url, true).query;
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Processing parameters can look like this
-    for(const [key, value] of Object.entries(params)) {
-        console.log(key + ": " + value);
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
+});
+
+app.get('/', (request, response) => {
+    pool.query('SELECT * FROM contacts ORDER BY id ASC', (error, results) => {
+        if(error) {
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    })
+});
+
+app.post('/add', (request, response) => {
+    const { name, email, phone } = request.body;
+    if(name && email && phone) {
+        pool.query('INSERT INTO contacts (name, email, phone) VALUES ($1, $2, $3)', [name, email, phone], (error, result) => {
+            if(error) {
+                throw error;
+            }
+            response.status(201).send('Contact added with id' + result.id);
+        })
+    } else {
+        console.log('Missing or empty parameters');
+        response.status(400);
     }
-    // You can write a webpage like this
-    response.write('Hello world!');
-    response.end();
-}
+
+});
+
+
+app.listen(port, () => {
+    console.log('App running on port ' + port);
+});
